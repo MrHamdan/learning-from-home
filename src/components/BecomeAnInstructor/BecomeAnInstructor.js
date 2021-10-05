d7e3e971bface8ba8766d4471d3a3e7ee8bac599
MrHamdan <mr.hamdanahmed@gmail.com>
2021-10-05 11:36:36 +0600

    Finished

--- src/components/BecomeAnInstructor/BecomeAnInstructor.js
+++ src/components/BecomeAnInstructor/BecomeAnInstructor.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, ButtonGroup, Card, CardGroup } from 'react-bootstrap';
+import { Button, Card, CardGroup } from 'react-bootstrap';
 import './BecomeAnInstructor.css'
 
 const BecomeAnInstructor = () => {
